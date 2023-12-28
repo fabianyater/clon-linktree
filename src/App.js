e0780fc7729ab220d1230e19a30a9892836a3ce6
Fabian Yate <fabianyate31@gmail.com>
2023-12-28 13:30:24 -0500

    Enhanced UI with background gradient and blur
    Introduced two new visual layers behind the main content of the application to improve user experience through a more dynamic and aesthetically pleasing interface. A subtle gradient grid and a blurred circular element have been added to establish depth and a modern look. Additionally, the background color variable has been updated to a darker shade to complement the new design elements and provide better contrast. These changes aim to enhance readability and overall visual appeal.
    
    Refs #1234

--- src/App.js
+++ src/App.js
@@ -16,37 +16,43 @@ function App() {
   }, [theme]);
 
   return (
-    <div className={styles.container} data-theme={theme}>
-      <header className={styles.header}>
-        <Avatar
-          fullname={User.name}
-          username={User.username}
-          description={User.description}
-        />
-      </header>
-      <main>
-        <ul className={styles.list}>
-          {Links.map((item, index) => {
-            return (
-              <li className={styles.listItem}>
-                <Link
-                  key={index}
-                  icon={item.icon}
-                  name={item.name}
-                  link={item.link}
-                  alt=""
-                ></Link>
-              </li>
-            );
-          })}
-        </ul>
-      </main>
-      <footer>
-        <button className={styles.button} onClick={toggleTheme}>
-          {theme === 'light' ? <SvgIcon icon={"lightTheme"} /> : <SvgIcon icon={"darkTheme"} />}
-        </button>
-      </footer>
-    </div>
+    <>
+      <div className={styles.bgTop}>
+        <div className={styles.bgBottom}>
+        </div>
+      </div>
+      <div className={styles.container} data-theme={theme}>
+        <header className={styles.header}>
+          <Avatar
+            fullname={User.name}
+            username={User.username}
+            description={User.description}
+          />
+        </header>
+        <main>
+          <ul className={styles.list}>
+            {Links.map((item, index) => {
+              return (
+                <li className={styles.listItem}>
+                  <Link
+                    key={index}
+                    icon={item.icon}
+                    name={item.name}
+                    link={item.link}
+                    alt=""
+                  ></Link>
+                </li>
+              );
+            })}
+          </ul>
+        </main>
+        <footer>
+          <button className={styles.button} onClick={toggleTheme}>
+            {theme === 'light' ? <SvgIcon icon={"lightTheme"} /> : <SvgIcon icon={"darkTheme"} />}
+          </button>
+        </footer>
+      </div>
+    </>
   );
 }
 
